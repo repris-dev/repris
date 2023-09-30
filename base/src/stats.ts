2f16420f3841ef51cfe2238b48f2fd10993b0670
ray glover <gh@rayglover.net>
2023-09-30 01:00:00 +0100

    stats exports

--- base/src/stats.ts
+++ base/src/stats.ts
@@ -1,2 +1,6 @@
 export { default as ReservoirSample } from './stats/ReservoirSample.js';
 export { default as OnlineStats, SimpleSummary } from './stats/OnlineStats.js';
+export * as kde from './stats/kde.js'
+export * as centralTendency from './stats/centralTendency.js';
+export * as mode from './stats/mode.js';
+export * from './stats/util.js';
