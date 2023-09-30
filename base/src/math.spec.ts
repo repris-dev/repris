9e40dbf2dfce0b3db81dbc401a656e790dfbe068
ray glover <gh@rayglover.net>
2023-09-30 01:00:00 +0100

    - Golden-section search - Rounded division

---
+++ base/src/math.spec.ts
@@ -0,0 +1,19 @@
+import * as math from './math.js';
+
+test('divRounded()', () => {
+  expect(math.divRounded(3n, 2n)).toBe(2n);
+  expect(math.divRounded(1500n, 1000n)).toBe(2n);
+  expect(math.divRounded(0n, 2n)).toBe(0n);
+  expect(math.divRounded(4n, 2n)).toBe(2n);
+  expect(math.divRounded(4n, 4n)).toBe(1n);
+});
+
+describe('gss', () => {
+  test('finds min', () => {
+    const f = (x: number) => Math.cos(x) * x;
+    const r = math.gss(f, -3.5, 1, 1e-7, 100);
+
+    const min = (r[0] + r[1]) / 2;
+    expect(min).toBeCloseTo(-0.86, 3);
+  });
+});
