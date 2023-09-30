ed61397f11e154431e79d07bae4d6bde0ab001cc
ray glover <gh@rayglover.net>
2023-09-30 01:00:00 +0100

    stats - validate sample data

--- base/src/stats/centralTendency.ts
+++ base/src/stats/centralTendency.ts
@@ -1,4 +1,5 @@
-import { Indexable } from "../util.js";
+import { assert } from '../index.js';
+import { Indexable } from '../util.js';
 
 export function mean(xs: Indexable<number>) {
   const norm = 1 / xs.length;
@@ -16,7 +17,8 @@ export function geometricMean(xs: Indexable<number>) {
 
   let sum = 0;
   for (let i = 0; i < xs.length; i++) {
-    sum += norm * Math.log(xs[i]);
+    assert.gte(xs[i], 0);
+    sum += norm * xs[i];
   }
 
   return Math.exp(sum);
@@ -25,6 +27,7 @@ export function geometricMean(xs: Indexable<number>) {
 export function harmonicMean(xs: Indexable<number>) {
   let sum = 0;
   for (let i = 0; i < xs.length; i++) {
+    assert.gte(xs[i], Number.EPSILON);
     sum += 1 / xs[i];
   }
 
