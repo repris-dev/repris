f1429752a43fb10e6f4c6208f93cb108cc55ae0b
ray glover <gh@rayglover.net>
2023-09-30 01:00:00 +0100

    delete unsed ./app

--- app/src/App.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test, expect } from "@playwright/test";
-
-test("Visual Regression Test", async ({ page }) => {
-  await page.goto("http://localhost:3000/");
-
-  expect(await page.screenshot()).toMatchSnapshot("vrt.png", {
-    threshold: 0.075,
-  });
-});
