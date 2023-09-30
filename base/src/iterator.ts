77c4bbfdf8b7772f1b45a59bc30209c50c5f1ebc
ray glover <gh@rayglover.net>
2023-09-30 01:00:00 +0100

    iterator utils

--- base/src/iterator.ts
+++ base/src/iterator.ts
@@ -55,3 +55,41 @@ export function returnLast<T>(iter: Iterator<T, any>, defaultValue: T): Iterator
 
   return { next };
 }
+
+/**
+ * Returns an iterable yielding all the values from the given iterables in
+ * order. If any iterables are infinite then none of the values from
+ * subsequent iterables will be read.
+ *
+ * @example
+ *
+ *   toArray(concat([range(0, 1), range(4, 5)]))
+ *   // [0, 1, 4, 5]
+ */
+export function* concat<T>(its: Array<Iterable<T>>): Iterable<T> {
+  for (const it of its) {
+    yield* it;
+  }
+}
+
+/**
+ * Take the first n elements of it
+ */
+export function* take<T>(n: number, it: Iterable<T>): Iterable<T> {
+  for (const x of it) {
+    if (n-- > 0) {
+      yield x;
+    } else {
+      break;
+    }
+  }
+}
+
+/**
+ * Create a generator
+ */
+export function* gen<T>(fn: () => T): Iterable<T> {
+  while (true) {
+    yield fn();
+  }
+}
