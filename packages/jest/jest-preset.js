a51d3a92f72405b264ccb884348dc9cb625f5ec8
ray glover <gh@rayglover.net>
2023-09-30 01:00:00 +0100

    allow --conditions=development to work with jest reporter resolution

--- packages/jest/jest-preset.js
+++ packages/jest/jest-preset.js
@@ -1,11 +1,17 @@
+import { createRequire } from 'node:module';
+
+// Resolving to actual paths ensure we use Node's own resolution algo
+// rather than Jests which can't be configured from here
+const resolve = createRequire(import.meta.url).resolve;
+
 /** @type {import('jest').Config} */
 export default {
   reporters: [
-    '@repris/jest/reporter',
-    '@repris/jest/summaryReporter',
+    resolve('@repris/jest/reporter'),
+    resolve('@repris/jest/summaryReporter'),
   ],
-  setupFilesAfterEnv: ['@repris/jest/stopwatch-env'],
-  testRunner: '@repris/jest/runner',
+  setupFilesAfterEnv: [resolve('@repris/jest/stopwatch-env')],
+  testRunner: resolve('@repris/jest/runner'),
   maxWorkers: 1,
   maxConcurrency: 1,
   testTimeout: 15_000,
