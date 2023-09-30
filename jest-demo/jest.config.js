683d0a03aa1a76818d3aad20ac524b5a2437c6cd
ray glover <gh@rayglover.net>
2023-09-30 01:00:00 +0100

    jest preset

--- jest-demo/jest.config.js
+++ jest-demo/jest.config.js
@@ -5,14 +5,9 @@ import pkg from './package.json' assert { type: 'json' };
 Object.assign(globalThis, defaults(pkg));
 
 export default {
+  preset: '@sampleci/jest',
   roots: ['<rootDir>/.tsc/'],
   testEnvironmentOptions: { customExportConditions: ['development'] },
   globals: defaults(pkg),
-  reporters: [
-    '@sampleci/jest/custom-reporter',
-  ],
-  setupFilesAfterEnv: ['@sampleci/jest/stopwatch-env'],
-  testRunner: '@sampleci/jest/custom-runner',
-  maxWorkers: 1,
   testRegex: '\\.tsc/.*\\.js$'
 };
---
+++ jest/jest-preset.js
@@ -0,0 +1,20 @@
+/** @type {import('jest').Config} */
+export default {
+  reporters: [
+    ['@sampleci/jest/custom-reporter', {
+      columns: [
+        { id: 'unknown' },
+        { id: 'duration:n', displayName: 'n' },
+        { id: 'duration:min', displayName: 'min' },
+        { id: 'mode:kde', displayName: 'kde' },
+        { id: 'mode:kde:dispersion', displayName: 'kde-d' },
+        { id: 'mode:hsm', displayName: 'hsm' },
+      ]
+    }],
+  ],
+  setupFilesAfterEnv: ['@sampleci/jest/stopwatch-env'],
+  testRunner: '@sampleci/jest/custom-runner',
+  maxWorkers: 1,
+  maxConcurrency: 1,
+};
+
