1660f953c651ba4339747cdacfbf69655e8dae5b
ray glover <gh@rayglover.net>
2023-09-30 01:00:00 +0100

    demo

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,8 +1,10 @@
-import { hello } from "@sampleci/base";
+import { array } from "@sampleci/base";
 import { useState } from "react";
 
 function App(): JSX.Element {
-  const [foo] = useState(hello());
+  const arr = [5, 4, 2, 8, 9];
+  const median = array.quickselect(arr, 2);
+  const [foo] = useState(median);
 
   return <h1>{JSON.stringify(foo)}</h1>;
 }
