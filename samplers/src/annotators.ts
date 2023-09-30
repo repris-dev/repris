4bade742ac3af948b629beeb2aa6bb411aa2aa60
ray glover <gh@rayglover.net>
2023-09-30 01:00:00 +0100

    exports correction

--- samplers/src/annotators.ts
+++ samplers/src/annotators.ts
@@ -1,3 +1,4 @@
 export * from './annotators/types.js';
 export * from './annotators/manager.js';
 export * as mode from './annotators/mode.js';
+export * as allPairs from './annotators/allPairs.js';
