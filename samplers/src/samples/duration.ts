97f2062437dd6f5f5ea170eb75817dcf6b47ae55
ray glover <gh@rayglover.net>
2023-09-30 01:00:00 +0100

    annotate relative margin of error

--- samplers/src/samples/duration.ts
+++ samplers/src/samples/duration.ts
@@ -32,14 +32,15 @@ export class Duration implements types.MutableSample<timer.HrTime> {
 
   private times: stats.ReservoirSample<timer.HrTime>;
 
-  // Todo: stats specifically for bigint
-  private onlineStats: stats.OnlineStats;
+  // TODO: stats specifically for bigint
+  private onlineStats: stats.LognormalOnlineStats;
 
-  constructor (private maxSize?: number) {
+  constructor (private maxCapacity?: number) {
     this.times = new stats.ReservoirSample(
-        maxSize === void 0 ? Number.MAX_SAFE_INTEGER : maxSize);
+      maxCapacity === void 0 ? Number.MAX_SAFE_INTEGER : maxCapacity
+    );
 
-    this.onlineStats = new stats.OnlineStats();
+    this.onlineStats = new stats.LognormalOnlineStats();
   }
 
   count() {
@@ -50,10 +51,10 @@ export class Duration implements types.MutableSample<timer.HrTime> {
     return this.times.values.values();
   }
 
-  summary(): stats.SimpleSummary<number> {
+  summary(): stats.LognormalOnlineStats {
     return this.onlineStats;
   }
-  
+
   reset() {
     this.times.reset();
     this.onlineStats.reset();
@@ -62,6 +63,27 @@ export class Duration implements types.MutableSample<timer.HrTime> {
   push(duration: timer.HrTime) {
     this.times.push(duration);
     this.onlineStats.push(Number(duration));
+  }
+
+  significant(): boolean {
+    return this.onlineStats.kurtosis() >= 0 && this.onlineStats.rme(1) < 5;
+
+//    const vals = this.times.values;
+//    const n = vals.length;
+//
+//    if (n < 3) { return false; }
+//
+//    const mode = this.onlineStats.mode();
+//
+//    let k = 0;
+//    for (let i = 0; i < n; i++) {
+//      if (Math.abs(Number(vals[i]) - mode) / mode < .25) { k++; };
+//    }
+//    
+//    console.info(mode, k, n);
+//    k /= (n - 1);
+//
+//    return k > .5;
   }
 
   toF64Array() {
@@ -83,8 +105,8 @@ export class Duration implements types.MutableSample<timer.HrTime> {
       units: 'microseconds' as quantity.Units
     } as WireType;
 
-    if (this.maxSize !== void 0) {
-      obj.maxSize = this.maxSize;
+    if (this.maxCapacity !== void 0) {
+      obj.maxSize = this.maxCapacity;
     }
 
     return obj;
@@ -94,7 +116,7 @@ export class Duration implements types.MutableSample<timer.HrTime> {
     if (!isDurationSampleWT(x)) { return invalidSample; }
 
     const sample = new Duration(x.maxSize);
-    sample.onlineStats = stats.OnlineStats.fromJson(x.summary);
+    sample.onlineStats = stats.LognormalOnlineStats.fromJson(x.summary);
 
     if (Array.isArray(x.values)) {
       x.values.forEach(v => sample.times.push(timer.fromString(v)));
@@ -139,6 +161,9 @@ const Annotations = {
 
   /** Coefficient of variation of all observations */
   cov: 'duration:cov' as typeid,
+
+  /** Relative margin of error */
+  rme95: 'duration:rme:95' as typeid,
 };
 
 const annotator = {
@@ -170,6 +195,7 @@ const annotator = {
       [Annotations.range, s.range() as ann.Annotation],
       [Annotations.min, s.range()[0]],
       [Annotations.max, s.range()[1]],
+      [Annotations.rme95, s.rme()],
     ]);
 
     return Status.value({ annotations, name: annotator.name });    
