f493b257cfd890ac9c0e41b79d59827ce7e3cfc8
ray glover <gh@rayglover.net>
2023-10-05 00:53:59 +0100

    benchmarks in production mode

--- packages/jest-demo/jest.config.js
+++ packages/jest-demo/jest.config.js
@@ -4,12 +4,16 @@ import pkg from './package.json' assert { type: 'json' };
 // Setup sampleci dev-env within the jest host (for the reporter)
 Object.assign(globalThis, defaults(pkg));
 
+// Simulate production repris (requires a full distribution build)
+const customExportConditions =
+  process.env.NODE_ENV === 'production' ? [] : ['development'];
+
 export default {
   preset: '@repris/jest',
   roots: ['<rootDir>/.tsc/'],
   testMatch: ['**/*.spec.[tj]s'],
   snapshotResolver: '@repris/util/snapshotResolver.cjs',
-  testEnvironmentOptions: { customExportConditions: ['development'] },
+  testEnvironmentOptions: { customExportConditions },
   globals: defaults(pkg),
   cacheDirectory: '<rootDir>/.jestcache/',
   transform: {},
