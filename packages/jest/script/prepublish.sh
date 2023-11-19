b9b5bb79496a1f1a45afc94bf4ea4eb987ea9b75
ray glover <gh@rayglover.net>
2023-11-19 17:49:05 +0000

    [ci] prepublish script for writing registry readme

---
+++ packages/jest/script/prepublish.sh
@@ -0,0 +1,11 @@
+# Package README (appears on the registry package homepage)
+cat >./README.md << 'EOF'
+<p align="center">
+  <a href="https://github.com/repris-dev/repris#readme">
+    <img src="https://repris.dev/logo.svg" width="200">
+  </a>
+</p>
+<p align="center">
+  <i>Reproducible benchmarking for <a href="jestjs.io">Jest</a></i>
+</p>
+EOF
