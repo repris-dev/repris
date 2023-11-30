9be0ec0fb0350f10679672537cddd0a6a2ef4d14
ray glover <gh@rayglover.net>
2023-11-30 01:15:06 +0000

    ppf approximation, mde calculation

--- packages/base/src/stats/normal.ts
+++ packages/base/src/stats/normal.ts
@@ -10,3 +10,47 @@ export function cdf(x: number, mean = 0, std = 1) {
 
   return z > 0 ? 1 - prob : prob;
 }
+
+function rationalApproximation(t: number): number {
+  // Abramowitz and Stegun formula 26.2.23.
+  // The absolute value of the error should be less than 4.5 e-4.
+  const c = [2.515517, 0.802853, 0.010328];
+  const d = [1.432788, 0.189269, 0.001308];
+  const numerator = (c[2] * t + c[1]) * t + c[0];
+  const denominator = ((d[2] * t + d[1]) * t + d[0]) * t + 1.0;
+  return t - numerator / denominator;
+}
+
+/** Percent point function */
+export function ppf(p: number): number {
+  // See article above for explanation of this section.
+  if (p < 0.5) {
+    // F^-1(p) = - G^-1(p)
+    return -rationalApproximation(Math.sqrt(-2.0 * Math.log(p)));
+  } else {
+    // F^-1(p) = G^-1(1-p)
+    return rationalApproximation(Math.sqrt(-2.0 * Math.log(1.0 - p)));
+  }
+}
+
+/**
+ * Returns the minimum detectable effect (MDE) for two samples at the given sensitivity and
+ * power level. See fundamentals of Biostatistics, Bernard Rosner - Equation 8.24 
+ * 
+ * @param a - desired significance level in a two-tailed test (e.g., for α = 0.05, Z1−α2≈1.96)
+ * @param b - desired statistical power
+ * @param n1 - first sample size
+ * @param n2 - second sample size
+ * @param std1 - first sample standard deviation
+ * @param std2 - second sample standard deviation
+ */
+export function mde(a: number, b: number, n1: number, std1: number, n2: number, std2: number) {
+  const Za = ppf((1 - a) / 2);
+  const Zb = ppf(1 - b);
+
+  const numerator = ((Za + Zb) ** 2) * ((std1 ** 2) + (std2 ** 2));
+  const mde1 = Math.sqrt(numerator / n1);
+  const mde2 = Math.sqrt(numerator / n2);
+
+  return (mde1 + mde2) / 2;
+}
