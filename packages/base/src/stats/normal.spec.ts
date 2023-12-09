d148f788b3672048c46517752b235d4d7e17e5c5
ray glover <gh@rayglover.net>
2023-12-09 13:53:53 +0000

    - pdf() - normal dist tests

---
+++ packages/base/src/stats/normal.spec.ts
@@ -0,0 +1,30 @@
+import { pdf, cdf, ppf } from './normal.js';
+
+describe('pdf', () => {
+  test('densities', () => {
+    expect(pdf(0, 0, 1)).toBeCloseTo(0.398, 2);
+    expect(pdf(1, 0, 1)).toBeCloseTo(0.241, 2);
+    expect(pdf(-1, 0, 1)).toBeCloseTo(0.241, 2);
+    expect(pdf(1, 1, 2)).toBeCloseTo(0.199, 2);
+  });
+});
+
+describe('cdf', () => {
+  test('probabilities', () => {
+    expect(cdf(0, 0, 1)).toBeCloseTo(0.5, 2);
+    expect(cdf(0.5, 0, 1)).toBeCloseTo(0.691, 2);
+    expect(cdf(0.5, 3, 1)).toBeCloseTo(0.006, 2);
+    expect(cdf(0.99, 0, 1)).toBeCloseTo(0.838, 2);
+  });
+});
+
+describe('ppf', () => {
+  test('percentiles', () => {
+    expect(ppf(0.5)).toBeCloseTo(0, 2);
+    expect(ppf(0.691)).toBeCloseTo(0.5, 2);
+    expect(ppf(0.006)).toBeCloseTo(-2.512, 2);
+    expect(ppf(0.99)).toBeCloseTo(2.326, 2);
+    expect(ppf(0)).toEqual(-Infinity);
+    expect(ppf(1)).toEqual(Infinity);
+  });
+});
--- packages/base/src/stats/normal.ts
+++ packages/base/src/stats/normal.ts
@@ -11,6 +11,13 @@ export function cdf(x: number, mean = 0, std = 1) {
   return z > 0 ? 1 - prob : prob;
 }
 
+/** A normal continuous random variable with given location/scale */
+export function pdf(x: number, mean = 0, std = 1) {
+  const m = std * Math.sqrt(2 * Math.PI);
+  const e = Math.exp(-Math.pow(x - mean, 2) / (2 * (std * std)));
+  return e / m;
+}
+
 function rationalApproximation(t: number): number {
   // Abramowitz and Stegun formula 26.2.23.
   // The absolute value of the error should be less than 4.5 e-4.
@@ -18,12 +25,12 @@ function rationalApproximation(t: number): number {
   const d = [1.432788, 0.189269, 0.001308];
   const numerator = (c[2] * t + c[1]) * t + c[0];
   const denominator = ((d[2] * t + d[1]) * t + d[0]) * t + 1.0;
-  return t - numerator / denominator;
+
+  return Number.isFinite(denominator) ? t - numerator / denominator : denominator;
 }
 
 /** Percent point function */
 export function ppf(p: number): number {
-  // See article above for explanation of this section.
   if (p < 0.5) {
     // F^-1(p) = - G^-1(p)
     return -rationalApproximation(Math.sqrt(-2.0 * Math.log(p)));
@@ -35,8 +42,8 @@ export function ppf(p: number): number {
 
 /**
  * Returns the minimum detectable effect (MDE) for two samples at the given sensitivity and
- * power level. See fundamentals of Biostatistics, Bernard Rosner - Equation 8.24 
- * 
+ * power level. See fundamentals of Biostatistics, Bernard Rosner - Equation 8.24
+ *
  * @param a - desired significance level in a two-tailed test (e.g., for α = 0.05, Z1−α2≈1.96)
  * @param b - desired statistical power
  * @param n1 - first sample size
@@ -48,7 +55,7 @@ export function mde(a: number, b: number, n1: number, std1: number, n2: number, 
   const Za = ppf((1 - a) / 2);
   const Zb = ppf(1 - b);
 
-  const numerator = ((Za + Zb) ** 2) * ((std1 ** 2) + (std2 ** 2));
+  const numerator = (Za + Zb) ** 2 * (std1 ** 2 + std2 ** 2);
   const mde1 = Math.sqrt(numerator / n1);
   const mde2 = Math.sqrt(numerator / n2);
 
